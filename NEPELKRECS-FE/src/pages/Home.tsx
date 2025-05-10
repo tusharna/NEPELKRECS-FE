b19b05142978f41e90d8f7b8db84c646b0324750
Tushar Awate <tushar.awate09@gmail.com>
2025-05-10 23:37:16 +0530

    Routing setup

---
+++ NEPELKRECS-FE/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+    return <>
+        <h1>My Home Page</h1>
+        <p>
+            Go to <Link to="/products">the list of products</Link>.
+        </p>
+    </>
+}
+export default Home
---
+++ NEPELKRECS-FE/src/pages/RootLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import MainNavaigation from "../components/MainNavaigation";
+import classes from './Root.module.css';
+
+function RootLayout() {
+    return (
+        <>
+            <MainNavaigation/>
+            <main className={classes.content}>
+                <Outlet /></main>            
+        </>
+    )
+}
+export default RootLayout
