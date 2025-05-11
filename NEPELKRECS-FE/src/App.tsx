7b5f6821c64dc02156717c7e3bb20dd6c23b156f
Tushar Awate <tushar.awate09@gmail.com>
2025-05-11 18:12:21 +0530

    dummy commit

--- NEPELKRECS-FE/src/App.tsx
+++ NEPELKRECS-FE/src/App.tsx
@@ -5,14 +5,16 @@ import './App.css'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import RootLayout from './pages/RootLayout'
+import ErrorComponet from './pages/error'
 
 const router = createBrowserRouter([
   {
     path: '/', element: <RootLayout />,
+    errorElement: <ErrorComponet />,
     children:
       [
-        { path: '/', element: <Home /> },
-        { path: '/products', element: <Products /> }
+        { index: true,element: <Home /> },
+        { path: 'products', element: <Products /> }
       ]
   },
 
---
+++ NEPELKRECS-FE/src/pages/error.tsx
@@ -0,0 +1,11 @@
+import MainNavigation from "../components/MainNavaigation"
+
+function ErrorComponet() {
+    return (
+        <>
+            <MainNavigation/>
+                <h1>404</h1 >
+    </>)
+}
+
+export default ErrorComponet
